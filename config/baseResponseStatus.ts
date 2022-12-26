02f3403a69ad931523bdd16dd5afdc211a8061f3
bbaekddo <bbaekddo100@naver.com>
2022-12-26 11:19:14 +0900

    :wrench: error response 수정

--- config/baseResponseStatus.ts
+++ config/baseResponseStatus.ts
@@ -3,14 +3,14 @@ export default {
     SUCCESS: {"isSuccess": true, "code": 1000, "message": "성공"},
     
     // Request error
-    SIGNUP_PHONE_LENGTH: {"isSuccess": false, "code": 2001, "message": "핸드폰 번호의 길이가 맞지 않습니다"},
-    SIGNUP_PHONE_TYPE  : {"isSuccess": false, "code": 2002, "message": "핸드폰 번호는 숫자만 입력해주세요"},
-    
+    USER_NOT_FOUND: {"isSuccess": false, "code": 2001, "message": "사용자를 찾을 수 없습니다"},
+    PRODUCT_NOT_FOUND: {"isSuccess": false, "code": 2002, "message": "상품을 찾을 수 없습니다"},
+
     
     // Response error
-    SIGNUP_REDUNDANT_NICKNAME: {"isSuccess": false, "code": 3001, "message": "중복된 닉네임입니다"},
-    SIGNUP_USER_NOT_FOUND    : {"isSuccess": false, "code": 3002, "message": "가입한 계정 정보를 찾을 수 없습니다"},
-    
+    USER_DUPLICATE_NAME: {"isSuccess": false, "code": 3001, "message": "중복된 이름입니다"},
+    USER_REVIEW_ALREADY_DONE: {"isSuccess": false, "code": 3002, "message": "기존에 작성한 리뷰가 있습니다"},
+
     //Connection, Transaction 등의 서버 오류
     DB_ERROR: {"isSuccess": false, "code": 4000, "message": "데이터 베이스 에러"}
 };
